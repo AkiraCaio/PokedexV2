3980a276da425e5866a65092f66d2f8ca1cef00d
AkiraCaio <caio_vini2006@hotmail.com>
2020-07-12 16:38:30 -0300

    Corrigido o problema de arquitetura MVVM

--- PokeApiV2/Modules/Home/HomeScreenViewController.swift
+++ PokeApiV2/Modules/Home/HomeScreenViewController.swift
@@ -48,20 +48,96 @@ class HomeScreenViewController: BaseViewController	 {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.configDelegateAndRegisterCells()
-        self.configScreen()
+        self.startScreen()
         self.configLayout()
     
     }
     
-    fileprivate func configScreen() {
-        self.viewModel.configScrollView(scrollView: self.scrollView)
-        self.viewModel.configSuperiorView(view: self.superiorView)
-        self.viewModel.configPokebolaImageView(imageView: self.pokebolaImageView)
-        self.viewModel.configMainTitleLabel(label: self.mainTitleLabel)
-        self.viewModel.configSearchBar(searchBar: self.searchBar)
-        self.viewModel.configCollectionView(collectionView: self.selectMenuCollectionView)
-        self.viewModel.configTableView(tableView: self.bottomTableView)
+    fileprivate func startScreen() {
+        self.configDelegateAndRegisterCells()
+        self.startScrollView()
+        self.startSuperiorView()
+        self.startPokebolaImageView()
+        self.startMainTitleLabel()
+        self.startSearchBar()
+        self.startCollectionView()
+        self.startTableView()
     }
     
+    fileprivate func startScrollView() {
+        scrollView.backgroundColor = ColorName.baseScreenMainBackgroundColor.color
+    }
+    
+    fileprivate func startSuperiorView() {
+        self.superiorView.backgroundColor = .white
+        
+        self.superiorView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        self.superiorView.cornerRadiusAndShadow(
+            cornerRadius: 30,
+            shadowColor: UIColor.gray.cgColor,
+            heightShadowOffset: 2,
+            shadowOpacity: 0.3
+        )
+    }
+    
+    fileprivate func startPokebolaImageView() {
+        self.pokebolaImageView.image = Asset.pokebolaTopMain.image
+    }
+    
+    fileprivate func startMainTitleLabel() {
+        self.mainTitleLabel.text = L10n.BaseScreen.Title.firstSection
+        self.mainTitleLabel.font = UIFont(font: FontFamily.CircularStd.black, size: 30)
+        self.mainTitleLabel.textColor = ColorName.baseScreenMainTextColor.color
+        self.mainTitleLabel.numberOfLines = 0
+        self.mainTitleLabel.textAlignment = .justified
+    }
+    
+    fileprivate func startSearchBar() {
+        let searchTextFieldHeight: CGFloat = 45
+        
+        self.searchBar.searchBarStyle = UISearchBar.Style.prominent
+        self.searchBar.placeholder = L10n.BaseScreen.SearchBar.placeholder
+        
+        self.searchBar.placeholderFontAndColor(
+            font: UIFont(font: FontFamily.CircularStd.book, size: 14),
+            textColor: ColorName.baseScreenMainTextColor.color.withAlphaComponent(0.4)
+        )
+        
+        self.searchBar.backgroundImage = UIImage()
+        
+        self.searchBar.setImage(Asset.buscarIcon.image, for: .search, state: .normal)
+        self.searchBar.searchTextPositionAdjustment = UIOffset(horizontal: 12, vertical: 0)
+        self.searchBar.setPositionAdjustment(UIOffset(horizontal: 18, vertical: 0), for: UISearchBar.Icon.search)
+        
+        // Logica para aumentar o tamanho do searchTextField, precisa primeiro aumentar o tamanho para depois arredondar os cantos
+        let image = ImageUtil.getImageWithColor (
+            color: ColorName.baseScreenMainBackgroundColor.color,
+            size: CGSize (
+                width: 1,
+                height: searchTextFieldHeight
+            )
+        )
+        searchBar.setSearchFieldBackgroundImage(image, for: .normal)
+        
+        searchBar.searchTextFieldCornerRadius(cornerRadius: searchTextFieldHeight/2)
+    }
+    
+    fileprivate func startCollectionView() {
+        self.selectMenuCollectionView.backgroundColor = .clear
+    }
+    
+    fileprivate func startTableView() {
+        self.bottomTableView.backgroundColor = .clear
+        self.bottomTableView.isScrollEnabled = false
+    }
+    
+    fileprivate func configDelegateAndRegisterCells() {
+          self.selectMenuCollectionView.delegate = self
+          self.selectMenuCollectionView.dataSource = self
+          self.selectMenuCollectionView.register(CategoriaCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+          
+          self.bottomTableView.delegate = self
+          self.bottomTableView.dataSource = self
+          self.bottomTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+      }
 }
--- PokeApiV2/Modules/Home/HomeScreenViewModel.swift
+++ PokeApiV2/Modules/Home/HomeScreenViewModel.swift
@@ -6,78 +6,11 @@
 //  Copyright © 2020 Caio Pinho. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 class HomeScreenViewModel {
     
     let menuChoice: [EscolhaMenuEnum] = [.Pokedex, .Moves, .Abilites, .Items, .Locations, .TypeCharts]
     let news: [NewsModel] = NewsUtil.createNews()
-    
-    func configScrollView(scrollView: UIScrollView) {
-        scrollView.backgroundColor = ColorName.baseScreenMainBackgroundColor.color
-    }
-    
-    func configSuperiorView(view: UIView) {
-        view.backgroundColor = .white
-        
-        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-        view.cornerRadiusAndShadow(
-            cornerRadius: 30,
-            shadowColor: UIColor.gray.cgColor,
-            heightShadowOffset: 2,
-            shadowOpacity: 0.3
-        )
-    }
-    
-    func configPokebolaImageView(imageView: UIImageView) {
-        imageView.image = Asset.pokebolaTopMain.image
-    }
-    
-    func configMainTitleLabel(label: UILabel) {
-        label.text = L10n.BaseScreen.Title.firstSection
-        label.font = UIFont(font: FontFamily.CircularStd.black, size: 30)
-        label.textColor = ColorName.baseScreenMainTextColor.color
-        label.numberOfLines = 0
-        label.textAlignment = .justified
-    }
-    
-    func configSearchBar(searchBar: UISearchBar) {
-        let searchTextFieldHeight: CGFloat = 45
-        
-        searchBar.searchBarStyle = UISearchBar.Style.prominent
-        searchBar.placeholder = L10n.BaseScreen.SearchBar.placeholder
-        
-        searchBar.placeholderFontAndColor(
-            font: UIFont(font: FontFamily.CircularStd.book, size: 14),
-            textColor: ColorName.baseScreenMainTextColor.color.withAlphaComponent(0.4)
-        )
-
-        searchBar.backgroundImage = UIImage()
-        
-        searchBar.setImage(Asset.buscarIcon.image, for: .search, state: .normal)
-        searchBar.searchTextPositionAdjustment = UIOffset(horizontal: 12, vertical: 0)
-        searchBar.setPositionAdjustment(UIOffset(horizontal: 18, vertical: 0), for: UISearchBar.Icon.search)
-                
-//        Logica para aumentar o tamanho do searchTextField, precisa primeiro aumentar o tamanho para depois arredondar os cantos
-        let image = ImageUtil.getImageWithColor (
-            color: ColorName.baseScreenMainBackgroundColor.color,
-            size: CGSize (
-                width: 1,
-                height: searchTextFieldHeight
-            )
-        )
-        searchBar.setSearchFieldBackgroundImage(image, for: .normal)
-        
-        searchBar.searchTextFieldCornerRadius(cornerRadius: searchTextFieldHeight/2)
-    }
-    
-    func configCollectionView(collectionView: UICollectionView) {
-        collectionView.backgroundColor = .clear
-    }
-    
-    func configTableView(tableView: UITableView) {
-        tableView.backgroundColor = .clear
-        tableView.isScrollEnabled = false
-    }
     
 }
